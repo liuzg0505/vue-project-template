27aaa03d363adae0ec1a1ddd7c2e7aa5f3dc9baa
liuzg0505 <2505847327@qq.com>
2017-09-27 20:23:30 +0800

    vue-project-template

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      Hello Vue
+      <hr>
+      {{name}}
+
+      <router-view></router-view>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data(){
+      return {
+          name: "张三"
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
